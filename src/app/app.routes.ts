e4219b82a945bfab5146b245f5fab64fedf3ad48
Joshua Brunke <seafortghost@gmail.com>
2025-06-03 18:14:32 +0200

    Changed folder structure, added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LandingComponent } from "./landing/landing.component";
+import { ImprintComponent } from "./imprint/imprint.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: LandingComponent },
+  { path: "imprint", component: ImprintComponent },
+  { path: "privacy", component: PrivacyComponent },
+  { path: "**", redirectTo: "" }
+];
--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
+  standalone: true,
   imports: [],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
@@ -9,3 +10,4 @@ import { Component } from '@angular/core';
 export class ImprintComponent {
 
 }
+  
