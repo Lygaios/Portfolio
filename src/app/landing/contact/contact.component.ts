db33502a361ffaee623bec71c942ee15c8d01df2
Joshua Brunke <seafortghost@gmail.com>
2025-06-09 20:21:29 +0200

    Added functionality and further styling to contact form

--- src/app/landing/contact/contact.component.ts
+++ src/app/landing/contact/contact.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+  
+  onSubmit() {
+    console.log(this.contactData);
+  }
 }
