8d04f4593a5ceecd5ee5e4d51ab107bcf4bad4ea
Joshua Brunke <seafortghost@gmail.com>
2025-06-09 21:10:04 +0200

    Added further logic and HTTP client to contact form

--- src/app/landing/contact/contact.component.ts
+++ src/app/landing/contact/contact.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -11,15 +12,45 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
     message: "",
   }
 
+mailTest = true;
+
+  post = {
+    endPoint: "https://joshuabrunke.com/sendMail.php",
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        "Content-Type": "'text/plain",
+        responseType: "text",
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.info("Mail test mode is enabled. Form data:", this.contactData);
+      console.info("Form submitted successfully, but no email will be sent due to mailTest being true.");
+      ngForm.resetForm();
     }
   }
 }
