9920202b71f3139db77d4f0ea53875c0ac767738
Joshua Brunke <seafortghost@gmail.com>
2025-06-10 20:41:59 +0200

    Added early setup of pricavy policy

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from "@ngx-translate/core";
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-privacy',
+  selector: "app-privacy",
   standalone: true,
   imports: [CommonModule, TranslateModule],
-  templateUrl: './privacy.component.html',
-  styleUrls: ['./privacy.component.scss']
+  templateUrl: "./privacy.component.html",
+  styleUrls: ["./privacy.component.scss"]
 })
-export class PrivacyComponent {
+export class PrivacyComponent implements OnInit {
+  constructor(
+    private translate: TranslateService,
+    private http: HttpClient
+  ) {}
+
+ngOnInit(): void {
+  const lang = this.translate.currentLang || this.translate.getDefaultLang() || this.translate.getBrowserLang() || "en";
+  this.loadPrivacyTranslations(lang);
+
+  // Also listen for manual language changes (e.g., if user toggles EN/DE)
+  this.translate.onLangChange.subscribe((event) => {
+    this.loadPrivacyTranslations(event.lang);
+  });
+}
+
+private loadPrivacyTranslations(lang: string): void {
+  this.http.get(`/i18n/privacy.${lang}.json`).subscribe((translations) => {
+    this.translate.setTranslation(lang, translations, true); // Merge into current translations
+  });
+}
 
 }
