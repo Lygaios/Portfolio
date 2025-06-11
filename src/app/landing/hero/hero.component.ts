23e1b6b7415635308e651ac2d2f45bc6b832db40
Joshua Brunke <seafortghost@gmail.com>
2025-06-11 10:59:18 +0200

    Set routerlink from hero section email address to contact section

--- src/app/landing/hero/hero.component.ts
+++ src/app/landing/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
