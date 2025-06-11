6b14e1e0a61c713723a070b32975476bd0da5c1c
Joshua Brunke <seafortghost@gmail.com>
2025-06-11 16:03:32 +0200

    Set button link to contact section in About section, styled button with hover effect

--- src/app/landing/about/about.component.ts
+++ src/app/landing/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
