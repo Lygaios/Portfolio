be7fae57735392828fe135459963821687fb311e
Joshua Brunke <seafortghost@gmail.com>
2025-07-22 08:02:25 +0200

    Enhance transition durations for smoother animations in portfolio items and navbar; update menu open/close timing for improved UX

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -53,7 +53,7 @@ export class NavbarComponent implements OnInit {
    */
   private openMenu(): void {
     this.isMenuOpen = true;
-    setTimeout(() => (this.showSlope1 = false), 300);
+    setTimeout(() => (this.showSlope1 = false), 500);
   }
 
   /**
@@ -63,7 +63,7 @@ export class NavbarComponent implements OnInit {
     setTimeout(() => {
       this.isMenuOpen = false;
       this.showSlope1 = false;
-    }, 300);
+    }, 500);
   }
 
   /**
